3366401f47a4c1a674ab08edb9f7fbbede1f3d0b
Alan Fleming <alalfleming@gmail.com>
2018-03-17 15:18:23 -0400

    initialized

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=> console.log(`Listening on port ${port}!!!`));
